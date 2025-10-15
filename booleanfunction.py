93bcbaf642510ea0768e54e25953d51154f13489
abdkgit <bashikabdul@gmail.com>
2025-10-15 19:45:07 +0530

    Add files via upload

---
+++ booleanfunction.py
@@ -0,0 +1,9 @@
+def isdivisible(x,y):
+  if x%y==0:
+     result= True
+  else:
+    result= False
+  return result 
+a=int(input("enter the value for a:"))
+b=int(input("enter the value for b:"))
+print(isdivisible(a,b))
